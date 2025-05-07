7c98daa40fe1d4e39e2d98df314d7496ecc30e47
Itandewy-Abad <86856140+Itandewy-Abad@users.noreply.github.com>
2025-05-07 10:37:53 -0600

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Definir las fechas en las que queremos hacer commits
-FECHAS=("2025-04-15" "2025-04-16" "2025-04-17" "2025-04-18" "2025-04-29" "2025-04-30" "2025-05-01" \
-        "2025-05-06")
+FECHAS=("2025-01-01" "2025-01-10" "2025-01-14" "2025-01-15" "2025-01-17" "2025-01-18" "2025-01-19" \
+        "2025-04-15" "2025-04-16" "2025-04-17" "2025-04-18" "2025-04-29" "2025-04-30" \
+        "2025-05-01" "2025-05-06")
 
 # Iterar sobre cada fecha y hacer varios commits para mayor intensidad
 for FECHA in "${FECHAS[@]}"; do
